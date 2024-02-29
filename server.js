3c9fd41e9a1a0907bb37d9e3b58918b217417c5e
h-reyes <reyes.haley23@gmail.com>
2024-02-28 22:55:53 -0500

    breakthrough with mongo and file tweeks

--- server.js
+++ server.js
@@ -1,3 +1,6 @@
+require('dotenv').config();
+// Configuration to database
+require('./config/database');
 const express = require('express');
 const path = require('path');
 const favicon = require('serve-favicon');
