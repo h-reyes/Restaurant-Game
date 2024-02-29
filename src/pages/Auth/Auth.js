17cb2f75a749a29940ed16a1656f1e58e9ea1a02
h-reyes <reyes.haley23@gmail.com>
2024-02-29 00:26:21 -0500

    signup form and css

--- src/pages/Auth/Auth.js
+++ src/pages/Auth/Auth.js
@@ -1,6 +1,10 @@
+import SignUpForm from "../../components/SignUpForm/SignUpForm";
 // Keep same format for each page
 export default function Auth (props) {
     return(
+        <>
         <h1>Authentication Page</h1>
+        <SignUpForm />
+        </>
     ) 
 }
